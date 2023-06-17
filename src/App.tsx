20e351fec33cfd7f415a43e13aa79ccb0eef5a2a
ahmadiqbal12 <rayahmad612@gmail.com>
2023-06-17 18:10:54 +0500

    add components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Watch from "./pages/Watch";
 
 function App() {
   return (
-    <div className="App">
-        <h1>App</h1>
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/watch/:id" element={<Watch />} />
+          </Routes>
+      </Router>
   );
 }
 
